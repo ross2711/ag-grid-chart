3625b2c75b45d1e8fc4e40a7eacc3d62904e8f07
ross <ross.fleming@serviceware.se>
2019-07-04 17:03:15 +0200

    columndefs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,37 +6,149 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  private columnDefs;
+  private defaultColDef;
+  private popupParent;
+  private processChartOptions;
+  private rowData: any;
 
-  // columnDefs = [
-  //   {
-  //     headerName: 'Make',
-  //     field: 'make',
-  //     sortable: true,
-  //     filter: true,
-  //     checkboxSelection: true
-  //   },
-  //   { headerName: 'Model', field: 'model', sortable: true, filter: true },
-  //   { headerName: 'Price', field: 'price', sortable: true, filter: true }
-  // ];
+  constructor() {
+    this.columnDefs = [
+      {
+        headerName: 'Make',
+        field: 'make',
+        chartDataType: 'category',
+        sortable: true,
+        filter: true,
+        checkboxSelection: true
+      },
+      {
+        headerName: 'Model',
+        field: 'model',
+        chartDataType: 'category',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'Year',
+        field: 'year',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'Engine',
+        field: 'engine',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'Price',
+        field: 'price',
+        sortable: true,
+        filter: true
+      }
+    ];
+    // this.defaultColDef = {
+    //   width: 200,
+    //   resizable: true
+    // };
 
-  columnDefs = [
-    { headerName: 'Make', field: 'make', rowGroup: true },
-    { headerName: 'Price', field: 'price' }
-  ];
+    this.rowData = [
+      {
+        make: 'Ferrari',
+        model: 'Monza',
+        price: 200000,
+        year: 2019,
+        engine: 5.0
+      },
+      {
+        make: 'Ferrari',
+        model: 'Portofino',
+        price: 250000,
+        year: 2018,
+        engine: 5.0
+      },
+      {
+        make: 'Ferrari',
+        model: 'GTC4 ',
+        price: 200000,
+        year: 2017,
+        engine: 5.0
+      },
+      {
+        make: 'Lamborghini',
+        model: 'Huracan',
+        price: 200000,
+        year: 2018,
+        engine: 5.0
+      },
+      {
+        make: 'Lamborghini',
+        model: 'Aventador',
+        price: 250000,
+        year: 2019,
+        engine: 5.0
+      },
+      {
+        make: 'Lamborghini',
+        model: 'Urus ',
+        price: 200000,
+        year: 2019,
+        engine: 5.0
+      }
+    ];
 
-  autoGroupColumnDef = {
-    headerName: 'Model',
-    field: 'model',
-    cellRenderer: 'agGroupCellRenderer',
-    cellRendererParams: {
-      checkbox: true
-    }
+    this.popupParent = document.body;
+    this.processChartOptions = function(params) {
+      var opt = params.options;
+      opt.title = { text: 'Cars for Sale' };
+      opt.legendPosition = 'bottom';
+      if (params.type === 'groupedBar') {
+        opt.xAxis.labelRotation = 0;
+      }
+      opt.seriesDefaults.tooltipRenderer = function(params) {
+        console.log(params);
+        var value = params.datum[params.yField];
+        // console.log(params.yField);
+        // console.log(value);
+        return `<b>${params.yField}</b>: ${value}`;
+      };
+      console.log(opt);
+      return opt;
+    };
+  }
+
+  onFirstDataRendered(params) {
+    var chartRangeParams = {
+      cellRange: {
+        rowStartIndex: 0,
+        rowEndIndex: 79,
+        columns: ['make', 'price', 'model']
+      },
+      chartType: 'groupedBar',
+      chartContainer: document.querySelector('#myChart'),
+      aggregate: true
+    };
+    params.api.chartRange(chartRangeParams);
+  }
+
+  gridOptions = {
+    // To enable charting in the grid
+    enableCharts: true,
+    // To allow users to create charts from a Range Selection and / or display the Chart Ranges in the grid
+    enableRangeSelection: true
   };
-
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
 }
+
+// columnDefs = [
+//   { headerName: 'Make', field: 'make', rowGroup: true },
+//   { headerName: 'Price', field: 'price' }
+// ];
+// autoGroupColumnDef = {
+//   headerName: 'Model',
+//   field: 'model',
+//   cellRenderer: 'agGroupCellRenderer',
+//   cellRendererParams: {
+//     checkbox: true
+//   }
+// };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import 'ag-grid-enterprise';
+import 'ag-grid-enterprise/chartsModule';
 
 @NgModule({
   declarations: [AppComponent],
