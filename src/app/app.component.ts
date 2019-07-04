6973af6cc0d433480e8cc9f548206d3ae1b91057
ross <ross.fleming@serviceware.se>
2019-07-04 15:07:40 +0200

    simple table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-grid-chart';
+  title = 'app';
+
+  columnDefs = [
+    {
+      headerName: 'Make',
+      field: 'make',
+      sortable: true,
+      filter: true,
+      checkboxSelection: true
+    },
+    { headerName: 'Model', field: 'model', sortable: true, filter: true },
+    { headerName: 'Price', field: 'price', sortable: true, filter: true }
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
 }
